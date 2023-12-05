5e3fe397e1d62d0d0a3c82544baee4a7c5f49d23
xchxn <seokregi@gmail.com>
2023-12-05 11:09:38 +0900

    eliminate key detect easter egg

--- src/main.py
+++ src/main.py
@@ -1,18 +1,18 @@
 from three_operations import ThreerBasicOperations
 
-from easter_egg import EasterEgg
+# from easter_egg import EasterEgg
 import threading
 
-# Create an instance of the EasterEgg class
-easter_egg = EasterEgg()
-# Function to detect the '5' key press
-def detect_keys(stop_event):
-    easter_egg.track_keys()
-# Start the key detection in a separate thread2
+# # Create an instance of the EasterEgg class
+# easter_egg = EasterEgg()
+# # Function to detect the '5' key press
+# def detect_keys(stop_event):
+#     easter_egg.track_keys()
+# # Start the key detection in a separate thread2
 stop_event = threading.Event()
-key_detection_thread = threading.Thread(target=detect_keys, args = (stop_event,))
-key_detection_thread.daemon = True
-key_detection_thread.start()
+# key_detection_thread = threading.Thread(target=detect_keys, args = (stop_event,))
+# key_detection_thread.daemon = True
+# key_detection_thread.start()
 
 
 while True:
