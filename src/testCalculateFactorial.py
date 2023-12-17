514617baa5057ee1475b7a6f4df46253f7e11161
hwangseonheon <dolphin33@jbnu.ac.kr>
2023-12-17 17:07:49 +0900

    feat: 팩토리얼 테스트 코드 추가 및 함수 수정

--- src/testCalculateFactorial.py
+++ src/testCalculateFactorial.py
@@ -2,37 +2,36 @@ from three_operations import ThreerBasicOperations
 import unittest
 
 class TestCalculateFactorial(unittest.TestCase):
-    def test_calculate_factorial(self):
-        infix = []
-        infix.append(7)
-        infix.append("!")
-        self.assertEqual(ThreerBasicOperations.calculateFactorial(infix), "= " + str(5040)) # 7! = 5040
 
-    def test_calculate_0_factorial(self):
-        infix = []
-        infix.append(0)
-        infix.append("!")
-        self.assertEqual(ThreerBasicOperations.calculateFactorial(infix), "= " + str(1)) # 0! = 1
+    def test_valid_input(self):
+        infix = [7, "!"]
+        result = ThreerBasicOperations.calculateFactorial(infix)
+        self.assertEqual(result, '= 5040') # 7! = 5040
 
-    def test_calculate_negative_factorial(self):
-        infix = []
-        infix.append(-1)
-        infix.append("!")
-        self.assertEqual(ThreerBasicOperations.calculateFactorial(infix), "[ERROR] Out Of Range") # 음수 팩토리얼
+    def test_zero_factorial(self):
+        infix = [0, "!"]
+        result = ThreerBasicOperations.calculateFactorial(infix)
+        self.assertEqual(result, '= 1') # 0! = 1
 
-    def test_more_than_two_count_factorial(self):
-        infix = []
-        infix.append(1)
-        infix.append(2)
-        infix.append("!")
-        self.assertEqual(ThreerBasicOperations.calculateFactorial(infix), "[ERROR] Input Error") # 두개 이상의 숫자가 입력
+    def test_negative_input(self):
+        infix = [-1, "!"]
+        result = ThreerBasicOperations.calculateFactorial(infix)
+        self.assertEqual(result, "[ERROR] Out Of Range") # 음수 팩토리얼
 
-    def test_not_int_factorial(self):
-        infix = []
-        infix.append("*")
-        infix.append("!")
-        self.assertEqual(ThreerBasicOperations.calculateFactorial(infix), "[SYETEM] ERROR! 정수가 아닌 값이 입력되었습니다.") # 정수가 아닌 값 입력
+    def test_invalid_input(self):
+        infix = [1, 2, "!"]
+        result = ThreerBasicOperations.calculateFactorial(infix)
+        self.assertEqual(result, "[ERROR] Input Error") # 두개 이상의 숫자가 입력
 
+    def test_non_number_input(self):
+        infix = ['*', '!']
+        result = ThreerBasicOperations.calculateFactorial(infix)
+        self.assertEqual(result, "[SYETEM] ERROR! 정수가 아닌 값이 입력되었습니다.") # 실수가 아닌 값 입력
+
+    def test_not_integer_number_input(self):
+        infix = [7.33, "!"]
+        result = ThreerBasicOperations.calculateFactorial(infix)
+        self.assertEqual(result, "[SYETEM] ERROR! 정수가 아닌 값이 입력되었습니다.") # 정수가 아닌 실수 값
 
 if __name__ == '__main__':
     unittest.main()
